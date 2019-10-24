6007c79da0882ccdd8259e20eefc9e20e735e900
rubenbc7 <rubenborbollacastelo@hotmail.com>
2019-10-24 08:38:47 -0700

    Agregar archivos de proyecto.

---
+++ Listbox/Color.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listbox
+{
+    class Color
+    {
+        public string Nombre { get; set; }
+        public string Hexa { get; set; }
+        public string RGB { get; set; }
+
+        public Color(string nombre, string hexa, string rgb)
+        {
+            this.Nombre = nombre;
+            this.Hexa = hexa;
+            this.RGB = rgb;
+
+        }
+        public override string ToString()
+        {
+            return this.Nombre + "(" + this.Hexa + ")";
+        }
+    }
+}
---
+++ Listbox/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Listbox
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Color> Colores = new ObservableCollection<Color>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Colores.Add(new Color("Rojo", "#FF0000", "(255,0,0)"));
+            Colores.Add(new Color("Verde", "#00FF00", "(0,255,0)"));
+            Colores.Add(new Color("Azul", "#0000FF", "(0,0,255)"));
+            lstColores.ItemsSource = Colores;
+
+        }
+
+        private void BtnNuevoColor_Click(object sender, RoutedEventArgs e)
+        {
+           Colores.Add(new Color(txtColor.Text, txtHexa.Text, txtRGB.Text) );
+           txtColor.Text="";
+            txtHexa.Text = "";
+            txtRGB.Text="";
+
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if(lstColores.SelectedIndex != -1)
+            {
+                Colores.RemoveAt(lstColores.SelectedIndex);
+            }
+        }
+
+        private void LstColores_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtColor_Editar.Text = Colores[lstColores.SelectedIndex].Nombre;
+        }
+    }
+}
